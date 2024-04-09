37108e54fec6fa668d496fa380df2805a5bab527
Martin  Fico <martin.fico@seznam.cz>
2024-04-09 20:55:44 +0200

    Postoupeno

--- lekce10.1.py
+++ lekce10.1.py
@@ -7,6 +7,11 @@ class Book:
         print("Title: {}".format(self.title))
         print("Author: {}".format(self.author))
         print("Pages: {}".format(self.pages))
+
+    def __str__(self):
+        return (f"Title: {self.title} author:{self.author} pages:{self.pages}")
 book1=Book("python Crash Course", "Eric Matthes", 624)
 book2 = Book("JavaScript: The Good parts", "Douglas Crockford", 170)
-print(book1+book2)
+print(book1)
+book1.showInfo()
+# print(book1+book2)
